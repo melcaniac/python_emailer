f4ceb1df1bb163d4c9835659ff0bf360db96c5fe
Matt Leggett <matthew.leggett@peoplefluent.com>
2021-10-08 14:31:27 -0400

    Added testNetD.sh

---
+++ testNetD.sh
@@ -0,0 +1,12 @@
+echo Testing Frankfurt
+python emailer.py -g frankfurt.mailservice.netdimensions.com -f eoc@peopleclick.com -t matt@peopleclick.com -s frankfurt.mailservice.netdimensions.com-test -d frankfurt.mailservice.netdimensions.com-test
+python emailer.py -g 10.6.40.25 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.6.40.25-test -d 10.6.40.25-test
+python emailer.py -g 10.6.40.26 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.6.40.26-test -d 10.6.40.26-test
+echo Testing Ohio
+python emailer.py -g ohio.mailservice.netdimensions.com -f eoc@peopleclick.com -t matt@peopleclick.com -s ohio.mailservice.netdimensions.com-test -d ohio.mailservice.netdimensions.com-test
+python emailer.py -g 10.5.40.25 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.5.40.25-test -d 10.5.40.25-test
+python emailer.py -g 10.5.40.26 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.5.40.26-test -d 10.5.40.26-test
+echo Testing Sydney
+python emailer.py -g sydney.mailservice.netdimensions.com -f eoc@peopleclick.com -t matt@peopleclick.com -s sydney.mailservice.netdimensions.com-test -d sydney.mailservice.netdimensions.com-test
+python emailer.py -g 10.7.40.25 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.7.40.25-test -d 10.7.40.25-test
+python emailer.py -g 10.7.40.26 -f eoc@peopleclick.com -t matt@peopleclick.com -s 10.7.40.26-test -d 10.7.40.26-test
