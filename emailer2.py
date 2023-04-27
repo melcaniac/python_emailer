c4fa56d2c314f2c8b87c080da7e4b7581a063a3a
Matt Leggett <matthew.leggett@peoplefluent.com>
2023-04-27 17:43:01 -0400

    WORKING SENDS TO MULTIPLE ADDRESSES!

--- emailer2.py
+++ emailer2.py
@@ -1,5 +1,8 @@
 #!/usr/local/bin/python
+# Import smtplib for the actual sending function.
+# Import argparse to be able to parse input parameters
 import smtplib, argparse
+#Just Handles basic text for the body of the message
 from email.mime.text import MIMEText
 
 parser = argparse.ArgumentParser(description='Send an email')
@@ -7,20 +10,29 @@ parser.add_argument('-g', help='gateway', dest='gw', required=True)
 parser.add_argument('-f', help='from', dest='from_addr', required=True)
 parser.add_argument('-t', help='to', dest='to_addr', required=True)
 parser.add_argument('-s', help='subject', dest='subject', required=True)
-parser.add_argument('-d', help='message text', dest='text', required=True)
+parser.add_argument('-m', help='message text', dest='message_text', required=True)
 
 args = parser.parse_args()
+config = vars(args)
+
+print ("Here's your configuration/request:")
+print (config)
+
+recipients = str(args.to_addr)
+mysender = str(args.from_addr)
 
 #setup the message
-msg = MIMEText(str(args.text))
+msg = MIMEText(str(args.message_text))
 msg['Subject'] = str(args.subject)
 msg['From'] = str(args.from_addr)
-msg['To'] = str(args.to_addr)
+msg['To'] = recipients
 
+print ("This will send to these recipients:")
+print (recipients)
+
+print (recipients.split(","))
 #send the message
-s = smtplib.SMTP(args.gw)
-s.sendmail(str(args.from_addr), [str(args.to_addr)], msg.as_string())
-
-s.set_debuglevel(1)
-
-s.quit()
+server = smtplib.SMTP(args.gw)
+server.send_message(msg, mysender, recipients.split(","))
+server.set_debuglevel(1)
+server.quit()
